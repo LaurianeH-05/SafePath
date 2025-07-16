09f6d4d900483f032b62e8179d5436ed9d9bd78a
Lory <147001347+LaurianeH-05@users.noreply.github.com>
2025-07-16 11:33:01 -0400

    report form completed

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
 
 app = Flask(__name__)
 
+#route to safety tips page
 @app.route('/tips')
 def tips():
     return render_template('tips.html')
+
+#route to show the form
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+#route to process the form
+@app.route('/submit_report', methods=['POST'])
+def submit_report():
+    location = request.form['location']
+    report_type = request.form['type']
+    description = request.form['description']
+
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    c.execute("INSERT INTO reports (location, type, description) VALUES (?, ?, ?)",
+              (location, report_type, description))
+    conn.commit()
+    conn.close()
+
+    return redirect('/report')
