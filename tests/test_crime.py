6e60d92c69fe9b3463a520c2467280d748b4ad66
ayushmp10 <ayushparambath@gmail.com>
2025-07-18 07:44:42 -0400

    fixed

--- tests/test_crime.py
+++ tests/test_crime.py
@@ -1,10 +1,5 @@
 import unittest
 from unittest.mock import patch, MagicMock
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from crime import get_crime_coordinates, client
 from sodapy import Socrata
 
